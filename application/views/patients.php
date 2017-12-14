b42e6e6090355ab3458c366cdc31b9d40876db72
Kenneth Lebu <kenlebu@gmail.com>
2017-12-14 10:34:05 +0300

    Add page for patient CRUD operations
    - Add a UI for CRUD operations for the patient

---
+++ application/views/patients.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kijiji Web Helper</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+
+<div class="row">
+  	<div class="col-lg-12 margin-tb">
+  	  <div class="pull-left">
+  	    <h2>Patients</h2>
+  	  </div>
+  	  <div class="pull-right">
+  	    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#create-patient"> Add patient</button>
+  	  </div>
+  	</div>
+</div>
+<table class="table table-bordered">
+
+	<thead>
+	    <tr>
+		      <th>Title</th>
+		      <th>Description</th>
+		      <th width="200px">Action</th>
+	    </tr>
+	</thead>
+	<tbody>
+	</tbody>
+
+</table>
+<ul id="pagination" class="pagination-sm"></ul>
+
+<!-- Create patient Modal -->
+<div class="modal fade" id="create-patient" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+        <h4 class="modal-title" id="myModalLabel">Create patient</h4>
+      </div>
+      <div class="modal-body">
+
+            <form data-toggle="validator" action="patients/store" method="POST">
+                <div class="form-group">
+                    <label class="control-label" for="firstname">Firstname:</label>
+                    <input type="text" name="firstname" class="form-control" data-error="Enter the patient's firstname." required />
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="lastname">Lastname:</label>
+                    <input type="text" name="lastname" class="form-control" data-error="Enter the patient's lastname." required />
+                    <div class="help-block with-errors"></div>
+                </div>
+                
+                <div class="form-group">
+                    <label class="control-label" for="dob">Date of birth:</label>
+                    <input type="text" name="dob" class="form-control" data-error="Enter the date of birth" required placeholder="YYYY-MM-DD" />
+                    <div class="help-block with-errors"></div>
+                </div>
+                
+                <div class="form-group col-xs-6">
+                <label class="css-input css-radio css-radio-default push-10-r">
+                    <input type="radio" name="gender"><span></span> Male
+                </label>
+                <label class="css-input css-radio css-radio-default">
+                    <input type="radio" name="gender"><span></span> Female
+                </label>
+            </div>
+                </div>
+
+                <!-- <div class="form-group">
+                    <label class="control-label" for="observation">Observations:</label>
+                    <textarea name="observation" class="form-control" data-error="Please enter observations." required></textarea>
+                    <div class="help-block with-errors"></div>
+                </div> -->
+                <div class="form-group">
+                    <button type="submit" class="btn crud-submit btn-success">Submit</button>
+                </div>
+
+            </form>
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- Edit patient Modal -->
+<div class="modal fade" id="edit-patient" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+        <h4 class="modal-title" id="myModalLabel">Edit patient</h4>
+      </div>
+
+      <div class="modal-body">
+            <form data-toggle="validator" action="" method="put">
+
+                <div class="form-group">
+                    <label class="control-label" for="title">Title:</label>
+                    <input type="text" name="title" class="form-control" data-error="Please enter title." required />
+                    <div class="help-block with-errors"></div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="title">Description:</label>
+                    <textarea name="description" class="form-control" data-error="Please enter description." required></textarea>
+                    <div class="help-block with-errors"></div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success crud-submit-edit">Submit</button>
+                </div>
+            </form>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.3.1/jquery.twbsPagination.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.5/validator.min.js"></script>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet">
+<script type="text/javascript">
+	var url = "patients";
+</script>
+
+
+</body>
+</html>
